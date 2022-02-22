5ebbacb0e583fb8e65fe8c18fa1196d6276055a0
scott967 <scott967@users.noreply.github.com>
2022-02-21 16:17:22 -1000

    Initial commit [script.extendedinfo]-5.6.0
    [script.extendedinfo]-5.6.0
    
    This is the version currently available in repo-scripts for Kodi Helix
    
    fix folder

---
+++ script.extendedinfo/plugin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf8 -*-
+
+# Copyright (C) 2015 - Philipp Temminghoff <phil65@kodi.tv>
+# This program is Free Software see LICENSE file for details
+
+from resources.lib import router
+
+router.run()
