b570151273aa6ef9096dda1e7f94417dd058ac7f
scott967 <scott967@users.noreply.github.com>
2026-07-06 13:56:02 -1000

    fix a decorator return

--- script.extendedinfo/resources/kutil131/busyhandler.py
+++ script.extendedinfo/resources/kutil131/busyhandler.py
@@ -77,12 +77,12 @@ class BusyHandler:
             result = None
             try:
                 result = func(cls, *args, **kwargs)
+                self.hide_busy()
+                return result
             except Exception:
                 utils.log(traceback.format_exc())
                 utils.notify("Busy Error", "please contact add-on author")
-            finally:
                 self.hide_busy()
-                return result
 
         return decorator
 
