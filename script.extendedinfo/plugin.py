98d3857c22fbe52d793ad8dba78417bc83e682ba
scott967 <scott967@users.noreply.github.com>
2026-09-17 14:54:51 -1000

    doc plugin.py

--- script.extendedinfo/plugin.py
+++ script.extendedinfo/plugin.py
@@ -1,6 +1,8 @@
 # Copyright (C) 2015 - Philipp Temminghoff <phil65@kodi.tv>
 # Modifications copyright (C) 2022 - Scott Smart <scott967@kodi.tv>
 # This program is Free Software see LICENSE file for details
+"""Module called by Runplugin() as executable in Kodi
+"""
 
 from resources.kutil131 import utils
 from resources.lib.pluginmain import Main
